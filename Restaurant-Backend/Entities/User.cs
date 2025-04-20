4e766bca8c3bce9fc320b27e58febb9b357d4f3c
tomas bava <tomasbava98@gmail.com>
2025-04-20 19:11:09 -0300

    Change user entity

--- Restaurant-Backend/Entities/User.cs
+++ Restaurant-Backend/Entities/User.cs
@@ -8,22 +8,13 @@ namespace Restaurant_Backend.Entities;
 /// </summary>
 [Table("Users")]
 public class User : EntityBase
-{
-    /// <summary>
-    /// Gets or sets the username of the user.
-    /// </summary>
+{    
     [Required]
     public string UserName { get; set; } = null!;
-
-    /// <summary>
-    /// Gets or sets the password of the user.
-    /// </summary>
+    
     [Required]
     public string Password { get; set; } = null!;
-
-    /// <summary>
-    /// Gets or sets the name of the user.
-    /// </summary>
+    
     [Required]
     public string Name { get; set; } = null!;
 }
