b2c5f14aa4b547f1d9e11ce98f4e215c8002c6fb
tomas bava <tomasbava98@gmail.com>
2025-06-05 15:04:05 -0300

    Add migrations - changes on TableSessions

---
+++ Restaurant-Backend/Migrations/20250604233758_ChangeOnEntities.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Restaurant_Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeOnEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "TableSessions");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "TableSessions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "TableSessions");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "TableSessions",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
